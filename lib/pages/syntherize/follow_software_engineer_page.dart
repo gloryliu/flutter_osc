7af086be8b9a63cb7ff42380bcdf4497c7028178
liuzhenrong <liuzhenrong@huobi.com>
2021-05-27 19:52:55 +0800

    关注列表布局

--- lib/pages/syntherize/follow_software_engineer_page.dart
+++ lib/pages/syntherize/follow_software_engineer_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osc/utils/assets_color.dart';
 
 // 关注页面
 class FollowSoftwareEngineersPage extends StatefulWidget {
@@ -41,7 +42,57 @@ class _FollowSoftwareEngineersPageState
             itemCount: 100,
             shrinkWrap: true,
             itemBuilder: (BuildContext context, int index) {
-              return Text('第$index条');
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black26,
+                    width: 0.5,
+                  ),
+                  borderRadius:
+                  BorderRadius.circular(5), // 也可控件一边圆角大小
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(60),
+                        child: Container(
+                          child: Image.network(
+                            'https://avatars.githubusercontent.com/u/10879203?v=4',
+                            width: 60,
+                            height: 60,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '张三',
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                      ),
+                      Text(
+                        'java作者',
+                        style: TextStyle(color: Colors.black26, fontSize: 12),
+                      ),
+                      FlatButton(
+                        onPressed: () {},
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              left: 10, top: 2, right: 10, bottom: 2),
+                          decoration: BoxDecoration(
+                            color: Color(color_ff24cf5f), // 边色与边宽度
+                            borderRadius:
+                            BorderRadius.circular(5), // 也可控件一边圆角大小
+                          ),
+                          child: Text(
+                            '+ 关注',
+                            style: TextStyle(color: Colors.white, fontSize: 14),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
             }),
       ),
     );
